e1f6b74a72525cafed9bbe0ed88f7f22b3e9a9b7
navin.k@rejolut.com <navin.k@rejolut.com>
2022-07-14 17:10:53 +0530

    ok

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3010;
-const path = require('path');
+const path = require("path");
 
-app.use(express.static('static'));
+app.use(express.static("static"));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve('pages/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("pages/index.html"));
 });
-app.get('/data', (req, res) => {
-  res.send({ message: 'success', data: { name: 'Navin' } });
+app.get("/data", (req, res) => {
+  res.send({ message: "success", data: { name: "Navin" } });
 });
-
+app.get("/about", (req, res) => {
+  res.send({ message: "success", data: { name: "About" } });
+});
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
